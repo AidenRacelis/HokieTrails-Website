aa46aee1193dec118f99213337b6e7cba883564c
AidenRacelis <aidenracelis@gmail.com>
2024-12-30 00:13:05 -0500

    Add images to the elements of Navbar
    npm installed svgr/webpack to handle .svg images, but may uninstall later. Need to figure how to display svg images.
    
    Added the icon svgs to the public directory.

--- frontend/hokie-trails/components/menu-bar.tsx
+++ frontend/hokie-trails/components/menu-bar.tsx
@@ -2,27 +2,34 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import Image from 'next/image';
 
 const NavBar = () => {
     
     const navLinks = [
         {
             name: "Map",
-            href: "/map"
+            href: "/map",
+            icon: "/public/map_icon.svg"
+            
         },
         {
             name: "Lodges",
-            href: "/lodges"
+            href: "/lodges",
+            icon: "/public/lodge_icon.svg"
+            
         },
 
         {
             name: "Saved",
-            href: "/saved"
+            href: "/saved",
+            icon: "/public/bookmark_icon.svg"
         },
 
         {
             name: "Profile",
-            href: "/profile"
+            href: "/profile",
+            icon: "/public/profile_icon.svg"
         }
     ]
 
@@ -33,6 +40,7 @@ const NavBar = () => {
                 {navLinks.map((item) => (
                     <li key={item.href}>
                     <Link href={item.href} className={pathname === item.href ? 'active' : ''}>
+                        <Image src = {item.icon} alt = "Icon" width= {24} height={24}></Image>
                         {item.name}
                     </Link>
                     </li>
