8c6ced828cabafe4288f3db3bd9e9d50ae0c209b
AidenRacelis <aidenracelis@gmail.com>
2025-01-11 00:12:43 -0500

    Add svg images properly
    Utilized next/Images properly and only added the .svg string file location instead of its actual relative location.

--- frontend/hokie-trails/components/menu-bar.tsx
+++ frontend/hokie-trails/components/menu-bar.tsx
@@ -4,32 +4,33 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 
+
 const NavBar = () => {
     
     const navLinks = [
         {
             name: "Map",
             href: "/map",
-            icon: "/public/map_icon.svg"
+            icon: "map_icon.svg"
             
         },
         {
             name: "Lodges",
             href: "/lodges",
-            icon: "/public/lodge_icon.svg"
+            icon: "lodge_icon.svg"
             
         },
 
         {
             name: "Saved",
             href: "/saved",
-            icon: "/public/bookmark_icon.svg"
+            icon: "bookmark_icon.svg"
         },
 
         {
             name: "Profile",
             href: "/profile",
-            icon: "/public/profile_icon.svg"
+            icon: "profile_icon.svg"
         }
     ]
 
@@ -40,7 +41,7 @@ const NavBar = () => {
                 {navLinks.map((item) => (
                     <li key={item.href}>
                     <Link href={item.href} className={pathname === item.href ? 'active' : ''}>
-                        <Image src = {item.icon} alt = "Icon" width= {24} height={24}></Image>
+                        <Image src = {item.icon} alt = "Icon" width= {30} height={30}></Image>
                         {item.name}
                     </Link>
                     </li>
